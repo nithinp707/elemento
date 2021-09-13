88cba6e590b45476aff033c57f21cee0148d1693
nithinpexp <nithin.p@experionglobal.com>
2021-09-13 16:47:58 +0530

    Initial commit

---
+++ app/src/main/java/com/zenora/elemento/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.zenora.elemento
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
